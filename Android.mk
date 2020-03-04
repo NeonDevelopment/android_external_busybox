74e6f603817d3d2670e315068fc9bae6ae82fbc3
khanfrd <khanfrd94@gmail.com>
2020-03-04 21:27:30 +0530

    busybox: fix compilation error
    Change-Id: Id3010e3be29d318d9bed256fcb635caab79c417b

--- Android.mk
+++ Android.mk
@@ -8,13 +8,14 @@ BIONIC_ICS := false
 BIONIC_L := true
 
 BUSYBOX_WARNING_HIDE := -Wno-error=implicit-function-declaration -Wno-implicit-function-declaration -Wno-implicit-fallthrough \
-			-Wno-sign-compare -Wno-format-overflow -Wno-shift-negative-value -Wno-logical-not-parentheses -Wno-return-type
+			-Wno-sign-compare -Wno-format-overflow -Wno-shift-negative-value -Wno-logical-not-parentheses -Wno-return-type \
+                        -Wno-maybe-uninitialized
 
 # Make a static library for regex.
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := android/regex/bb_regex.c
 LOCAL_C_INCLUDES := $(BB_PATH)/android/regex
-LOCAL_CFLAGS := -Wno-sign-compare
+LOCAL_CFLAGS := -Wno-sign-compare -Wno-unused-variable -Wno-unused-parameter
 LOCAL_MODULE := libclearsilverregex
 include $(BUILD_STATIC_LIBRARY)
 
